40b1977c647cb9d06bc95eb8ca37da58785080c2
saitutiyu <uugu@163.com>
2022-04-23 18:04:59 +0800

    Update index.py

--- index.py
+++ index.py
@@ -2,15 +2,14 @@
 import action
 
 infos = {
-    "phone": "13355379535",
-    "password": "9103b4e46d7aa692e7e98f36ebf7d778",
+    "phone": "",
+    "password": "",
     # "sc_key": ["XXXX"],
     # "tg_bot_key": ["XXXX", "XXXXX"],
     # "bark_key": ["XXXX"],
     # "wecom_key": ["XXXX", "XXXX", "XXXX"],
-     "push_plus_key": ["35629a7229e340f196f353a7fef0878a"],
+    # "push_plus_key": ["XXXX"],
 }
-
 
 def main_handler(event, context):
     action.task_pool(infos)
